1255a01ce8b00a8d11519c5dade479a5710c3a54
Pierre Pasquier <pierrepas@yahoo.fr>
2020-06-30 11:29:10 +0200

    first commit

---
+++ src/Controller/PagesController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
+
+class PagesController extends AbstractController
+{
+    /**
+     * @Route("/", name="app_home")
+     */
+    public function home()
+    {
+        return $this->render('pages/home.html.twig');
+    }
+}
